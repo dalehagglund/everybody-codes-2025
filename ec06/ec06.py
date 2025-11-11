82132a0f4e57faf9708ad49bde721a31b6e864c4
Dale Hagglund <dale.hagglund@gmail.com>
2025-11-11 02:06:30 -0700

    ec06 part 3 solved

--- ec06/ec06.py
+++ ec06/ec06.py
@@ -3,6 +3,8 @@ import re
 from functools import partial, reduce
 from dataclasses import dataclass
 from collections import defaultdict
+from bisect import bisect_left, bisect_right
+import pytest
 
 def read_input(fname):
     with open(fname) as f:
@@ -31,8 +33,56 @@ def part2(input):
 def prod(items):
     return reduce(lambda x, y: x * y, items, 1)
     
+def find_interval(items, lower, upper):
+    left, right = bisect_left(items, lower), bisect_right(items, upper)
+    if left == len(items): return (0, 0) # empty range
+    assert right is not None
+    return left, right
+    
+
+@pytest.mark.parametrize(
+    "items, lower, upper, expected", [
+        ([], 3, 5, (0, 0)),
+        ([1, 1, 3, 11], 0, 100, (0, 4)),
+        ([1, 1, 1, 2, 2], 1, 1, (0, 3)),
+        ([1, 1, 1, 2, 2], 2, 2, (3, 5)),
+        ([1, 2, 3, 4, 5, 6, 7, 8], 2, 5, (1, 5)),
+        ([1, 2, 3, 4, 5, 6, 7, 8], 10, 10, (0, 0)),
+        ([1, 2, 3, 4, 5, 6, 7, 8], -2, -1, (0, 0)),
+        ([1, 2, 3, 4, 5, 6, 7, 8], -2, 3, (0, 3)),
+        ([1, 2, 3, 4, 5, 6, 7, 8], -2, 10, (0, 8)),
+            
+    ]
+)    
+def test_find_interval(items, lower, upper, expected):
+    assert find_interval(items, lower, upper) == expected
+
 def part3(input):
-    return None
+    mentors = defaultdict(list)
+    novices = defaultdict(list)
+
+    mindist = 1000
+    reps = 1000
+
+    for pos, ch in enumerate(input * reps):
+        if ch in "ABC": mentors[ch].append(pos)
+        else: novices[ch].append(pos)
+
+    # position lists are sorted by construction
+
+    total = 0
+    for category in "abc":
+        print(f"cat {category}:")
+        for npos in novices[category]:
+            left, right = find_interval(
+                mentors[category.upper()],
+                npos - mindist,
+                npos + mindist
+            )
+            #print(f"... nov {npos} {left, right = }")
+            total += right - left
+    
+    return total
     
 dispatch = {
     1: part1,
