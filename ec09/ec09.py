885d6ddc9fbe84267c270f8ac5eb4bf70eb03c68
Dale Hagglund <dale.hagglund@gmail.com>
2025-11-16 07:53:43 -0700

    ec09 part 3 solved

--- ec09/ec09.py
+++ ec09/ec09.py
@@ -6,8 +6,14 @@ from dataclasses import dataclass
 from collections import defaultdict, deque, Counter
 from bisect import bisect_left, bisect_right
 import pytest
-from itertools import pairwise, count, groupby, combinations
-from typing import NamedTuple
+from itertools import (
+    pairwise,
+    count,
+    groupby,
+    combinations,
+    product
+)
+from typing import NamedTuple, Iterator
 
 def star(f): lambda t: f(*t)
 
@@ -82,48 +88,156 @@ def part2v1(input):
     return total
         
 
-def part2_v2(input):
-    dna = [d for _, d in input]
+# def part2_v2(input):
+    # dna = [d for _, d in input]
     
-    def possible_child(d1, d2, d3):
-        return all(
-            a == b or a == c 
-            for a, b, c
-            in zip(d1, d2, d3)
-        )
+    # def possible_child(d1, d2, d3):
+        # return all(
+            # a == b or a == c 
+            # for a, b, c
+            # in zip(d1, d2, d3)
+        # )
         
-    def possible_family(a, b, c):
-        return (
-            possible_child(a, b, c)
-            or possible_child(b, a, c)
-            or possible_child(c, a, b)
-        )
+    # def possible_family(a, b, c):
+        # return (
+            # possible_child(a, b, c)
+            # or possible_child(b, a, c)
+            # or possible_child(c, a, b)
+        # )
 
-    def similarity(d1, d2):
-        return sum(a == b for a, b in zip(d1, d2))
+    # def similarity(d1, d2):
+        # return sum(a == b for a, b in zip(d1, d2))
 
 
-    groups = defaultdict(list)
-    for i, d in enumerate(dna):
-        groups[d[2:3]].append(i)
-    print(groups)
-    candidates = []
-    total_combos = 0
-    families = 0
-    for triple in combinations(groups.keys(), 3):
-        if possible_family(*triple):
-            families += 1
-            n = prod(map(lambda g: len(groups[g]), triple))
-            print(f'... {triple} {n}')
-            total_combos += n
-    print(f"{families = } {total_combos = }")
+    # groups = defaultdict(list)
+    # for i, d in enumerate(dna):
+        # groups[d[2:3]].append(i)
+    # print(groups)
+    # candidates = []
+    # total_combos = 0
+    # families = 0
+    # for triple in combinations(groups.keys(), 3):
+        # if possible_family(*triple):
+            # families += 1
+            # n = prod(map(lambda g: len(groups[g]), triple))
+            # print(f'... {triple} {n}')
+            # total_combos += n
+    # print(f"{families = } {total_combos = }")
 
 def prod(items):
     return reduce(lambda x, y: x * y, items, 1)
     
 def part3(input):
-    return None
+    dna = [d for _, d in input]
+    
+    @dataclass(frozen=True)
+    class Family:
+        parents: frozenset[int]
+        child: int
         
+        def __init__(self, ch, p1, p2):
+            # `object.__setattr__` to bypass frozen checks
+            object.__setattr__(self, "parents", frozenset({p1, p2}))
+            object.__setattr__(self, "child", ch)
+    
+        def __repr__(self): return str(self)
+        def __str__(self):
+            return f"{set(self.parents)} -> {self.child}"
+    
+    def possible_child(ch, p1, p2):
+        d1, d2, d3 = dna[ch], dna[p1], dna[p2]
+        return all(
+            a == b or a == c 
+            for a, b, c
+            in zip(d1, d2, d3)
+        )
+
+    def possible_family(a, b, c):
+            if possible_child(a, b, c): return a, b, c
+            if possible_child(b, a, c): return b, a, c
+            if possible_child(c, a, b): return c, a, b
+            return None
+
+    def similarity(d1, d2):
+        return sum(a == b for a, b in zip(d1, d2))
+    
+    families = set()
+    for loops, triple in enumerate(combinations(range(len(dna)), 3)):
+        if loops % 100000 == 0: print(f"... {loops:-8}")
+        i, j, k = triple
+        result = possible_family(i, j, k)
+        if result is None:
+            continue
+        fam = Family(*result)
+        # ch, p1, p2 = result
+        # s = similarity(ch, p1) * similarity(ch, p2)
+        # total += s
+        print(f"...    possible {i, j, k = } {fam = }")
+        families.add(fam)
+    #print(f"{families = }")
+    
+    combined: dict[frozenset[int], set[int]] = defaultdict(set)
+    for f in families:
+        combined[f.parents].add(f.child)
+    
+    print("combined:")
+    for parents, children in combined.items():
+        print(f"... {set(parents)} - > {children}")
+    
+    # confirm each id
+    # - shows up as a child at most once
+    # - shows up as a parent at most once
+
+    for i in range(len(dna)):
+        assert sum(i in children for children in combined.values()) <= 1
+
+    # for i in range(len(dna)):
+        # assert sum(i in parents for parents in combined.keys()) <= 1
+        
+    # i'm pretty sure that related can be directly  oomputed
+    # from the families as they're discovered.
+
+    related: dict[int, set[int]] = defaultdict(set)
+    for parents, children in combined.items():
+        for id1, id2 in product(parents | children, repeat=2):
+            related[id1].add(id2)
+
+    print("related:")
+    for id in range(len(dna)):
+        if id not in related: continue
+        print(f"... {id:3d} - > {', '.join(str(f) for f in sorted(related[id]))}")
+        
+    def bfs(start) -> Iterator[int]:
+        q = deque()
+        visited: set[int] = set()
+        
+        print(f"... bfs({start})")
+        push = q.append
+        pop = q.popleft
+        push(start)
+        while len(q) > 0:
+            n = pop()
+            if n in visited:
+                continue
+            visited.add(n)
+            yield n
+            for i in related[n]:
+                push(i)
+        
+    nodes = set(related.keys())
+    components: set[frozenset[int]] = set()
+    
+    while len(nodes) > 0:
+        start = nodes.pop()
+        cc = frozenset(bfs(start))
+        components.add(cc)
+        nodes -= cc
+    
+    print("components:")
+    for cc in components:
+        print(f"... {set(cc)}")
+
+    return sum(i+1 for i in max(components, key=len))
 dispatch = {
     1: part1,
     2: part2v1,
